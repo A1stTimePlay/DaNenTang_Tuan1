56f95f360266019d3be7ad032fa8fbdb98e44fe6
NguyenXuanAn <nguyenxuanan.nxa@gmail.com>
2019-10-03 23:28:35 +0700

    Add project files.

---
+++ App1/App1/MH_TinhTong.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App1
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MH_TinhTong : ContentPage
+    {
+        public MH_TinhTong()
+        {
+            InitializeComponent();
+        }
+
+        private void CmdTinhTong_Clicked(object sender, EventArgs e)
+        {
+            double SoA, SoB, Tong;
+            SoA = double.Parse(txtSoA.Text);
+            SoB = double.Parse(txtSoB.Text);
+            Tong = SoA + SoB;
+            txtTong.Text = Tong.ToString();
+        }
+
+        private void CmdVeTrangChu_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
